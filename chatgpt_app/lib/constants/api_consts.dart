2c533ab887fc07f0666f1ba54ee9c374f8dc33cd
LucasRomanato0 <lucasromanato34@gmail.com>
2023-07-25 13:11:15 -0300

    implementando o models

--- chatgpt_app/lib/constants/api_consts.dart
+++ chatgpt_app/lib/constants/api_consts.dart
@@ -1,3 +1,3 @@
 String BASE_URL = "https://api.openai.com/v1/";
 
-String API_KEY = "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";
+String API_KEY = "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";
--- chatgpt_app/lib/services/api_services.dart
+++ chatgpt_app/lib/services/api_services.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:chatgpt_app/constants/api_consts.dart';
+import 'package:chatgpt_app/models/models_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServices {
-  static Future<void> getModels() async {
+  static Future<List<ModelsModel>> getModels() async {
     try {
       var response = await http.get(
         Uri.parse("$BASE_URL/models"),
@@ -15,13 +17,21 @@ class ApiServices {
       Map jsonResponse = jsonDecode(response.body);
 
       if (jsonResponse['error'] != null) {
-        print('jsonResponse["error"]: ${jsonResponse['error']['message']}');
+        // print('jsonResponse["error"]: ${jsonResponse['error']['message']}');
         throw HttpException(jsonResponse['error']['message']);
       }
 
-      print('jsonResponse: $jsonResponse');
+      // print('jsonResponse: $jsonResponse');
+      List temp = [];
+      for (var value in jsonResponse["data"]) {
+        temp.add(value);
+        // print("temp ${value["id"]}");
+      }
+
+      return ModelsModel.modelsFromSnapshot(temp);
     } catch (error) {
       print('error $error');
+      rethrow;
     }
   }
 }
